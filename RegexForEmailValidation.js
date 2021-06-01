4f9d20514f7c063b95508e008c2ef7ef0f781a37
Pratiksha0401 <nagoshepratiksha@gmail.com>
2021-06-01 07:20:40 +0530

    Regex to validate email's 1st part i.e before of @

---
+++ RegexForEmailValidation.js
@@ -0,0 +1,11 @@
+const readline = require("prompt-sync");
+const prompt=readline();
+
+let email = prompt("Enter Email : ");
+
+const emailRegex = RegExp('^[A-Z a-z 0-9]{3,}[A-Z a-z 0-9 !@#$%&*.]*$');
+
+if(emailRegex.test(email))
+	console.log("Valid Pincode!");
+else
+	console.log("Invalid Pincode!");
