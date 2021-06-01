bd64c72f33b1060ddd7846934f019f220701d147
Pratiksha0401 <nagoshepratiksha@gmail.com>
2021-06-01 06:56:08 +0530

    Added : cpde to restricts taking alphabets or special characters at beginning

--- RegexForPINValidation.js
+++ RegexForPINValidation.js
@@ -6,7 +6,7 @@ let pin = prompt("Enter PIN : ");
 let pinRegex = RegExp('^[1-9]{1}[0-9]{5}$');
 
 if(pinRegex.test(pin))
-	console.log("Valid Pincode");
+	console.log("Valid Pincode !");
 else
-	console.log("Invalid Pincode");
+	console.log("Invalid Pincode !");
    
