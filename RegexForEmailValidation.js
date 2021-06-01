d21e7fae5b7d8fe531e3a8ffc1245e0b7bd20713
Pratiksha0401 <nagoshepratiksha@gmail.com>
2021-06-01 07:49:35 +0530

    Added : cod eto validate only 4 special chars _,-,+,.

--- RegexForEmailValidation.js
+++ RegexForEmailValidation.js
@@ -3,7 +3,7 @@ const prompt=readline();
 
 let email = prompt("Enter Email : ");
 
-const emailRegex = RegExp('^[A-Z a-z 0-9 !#$%&*.]{1,}[@]{1}[A-Z a-z 0-9 !#$%&*]{1,}[.]{1}[a-z]{2,3}[A-Z a-z 0-9 !#$%&]*$');
+const emailRegex = RegExp('^[A-Z a-z]{1,}([+-_.]*)[A-Z a-z 0-9 _+-.]*[@]{1}[A-Z a-z 0-9 +_-]{1,}[.]{1}[a-z]{2,3}[A-Z a-z .]*$');
 
 if(emailRegex.test(email))
 	console.log("Valid Pincode!");
